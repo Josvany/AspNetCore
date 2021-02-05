1457c0a28ef71bc2552801224ff8b3f46a75f40c
Josvany_G <Josvany_G@JOSVANY>
2021-02-05 11:06:30 -0600

    Agregar archivos de proyecto.

---
+++ AspNetCore/Data/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Models;
+
+namespace AspNetCore.Data
+{
+    public class DbInitializer
+    {
+        public static void Initializt (AspNetCoreContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+            var categorias = new Categoria[]
+            {
+                new Categoria { categoriaName = "Programacion",
+                                catgoriaDescription = "Programacion",
+                                categoriaStatus = true
+                              },
+
+                new Categoria { categoriaName = "Diseño Grafico",
+                                catgoriaDescription = "Diseño Grafico",
+                                categoriaStatus = true
+                              }
+            };
+
+            foreach (Categoria item in categorias)
+            {
+                context.Add(item);
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ AspNetCore/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public Guid categoriaIntId { get; set; }
+        [Required (ErrorMessage ="Requerido")]
+        public string categoriaName { get; set; }
+        public string catgoriaDescription { get; set; }
+        public bool categoriaStatus { get; set; }
+
+    }
+}
